72b52bde7b1c7954ee5ab8ad4dba1fa3d6f058ec
578675581@qq.com <578675581@qq.com>
2016-03-29 03:31:21 +0800

    creat a new project

---
+++ WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by turingx on 16/3/29.
+//  Copyright © 2016年 turingx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by turingx on 16/3/29.
+//  Copyright © 2016年 turingx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
